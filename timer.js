53b7a3fa7250e6728087e0ebadc78441a802d222
gavinblair <gavin.blair@rtraction.com>
2013-07-05 16:04:41 -0400

    Update timer.js

--- timer.js
+++ timer.js
@@ -1,12 +1,19 @@
 var countdown = function(from, updateCallback, finishedCallback){
   this.current = from;
   var t = this;
-  t.interval = setInterval(function(){
-    updateCallback(t.current);
-    if(t.current <= 0){
-      clearInterval(t.interval);
-      finishedCallback();
-    }
-    t.current --;
-  }, 1000);
+  this.resume();
+  
+  this.pause = function(){
+    clearInterval(this.interval);
+  };
+  this.resume = function(){
+    this.interval = setInterval(function(){
+      updateCallback(t.current);
+      if(t.current <= 0){
+        clearInterval(t.interval);
+        finishedCallback();
+      }
+      t.current --;
+    }, 1000);
+  };
 };
