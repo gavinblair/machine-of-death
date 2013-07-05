5a8ee43dc6c19106d9ffee6fca8f3b5399bcf64d
gavinblair <gavin.blair@rtraction.com>
2013-07-05 14:03:02 -0400

    Update game.js

--- game.js
+++ game.js
@@ -68,10 +68,10 @@ var game = function(difficulty){
 		//start a 90 second timer if it hasn't already started
 		this.timer = new countdown(90, function(t){
 			//update
-			document.getElementByTagName("h1").innerHTML = t+" seconds left!";
+			document.getElementsByTagName("h1")[0].innerHTML = t+" seconds left!";
 		}, function(){ 
 			//done
-			document.getElementByTagName("h1").innerHTML = "Time's up!";
+			document.getElementsByTagName("h1")[0].innerHTML = "Time's up!";
 		});
 		
 		document.body.innerHTML = ich.execute({hand: this.newGame.giftCards.hand});
